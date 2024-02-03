63183825306d95e01c5e9d55305a7841c4c982b8
Rhydian Jenkins <rhydian.jenkins@basekit.com>
2024-02-03 13:53:59 +0000

    hardcoded some goto-definition paths

--- README.md
+++ README.md
@@ -7,3 +7,49 @@ This is as much a learning exercise for me as it is a tool that I am going to us
 ```sh
 printf "Content-Length: 85\r\n\r\n{\"jsonrpc\": \"2.0\", \"method\": \"initialize\", \"id\": 1, \"params\": {\"capabilities\": {}}}" | cargo run
 ```
+
+## Intro
+
+A minimal example LSP server that can only respond to the `gotoDefinition` request. To use
+this example, execute it and then send an `initialize` request.
+
+```
+Content-Length: 85
+
+{"jsonrpc": "2.0", "method": "initialize", "id": 1, "params": {"capabilities": {}}}
+```
+
+This will respond with a server response. Then send it a `initialized` notification which will
+have no response.
+
+```
+Content-Length: 59
+
+{"jsonrpc": "2.0", "method": "initialized", "params": {}}
+```
+
+Once these two are sent, then we enter the main loop of the server. The only request this
+example can handle is `gotoDefinition`:
+
+```
+Content-Length: 159
+
+{"jsonrpc": "2.0", "method": "textDocument/definition", "id": 2, "params": {"textDocument": {"uri": "file://temp"}, "position": {"line": 1, "character": 1}}}
+```
+
+To finish up without errors, send a shutdown request:
+
+```
+Content-Length: 67
+
+{"jsonrpc": "2.0", "method": "shutdown", "id": 3, "params": null}
+```
+
+The server will exit the main loop and finally we send a `shutdown` notification to stop
+the server.
+
+```
+Content-Length: 54
+
+{"jsonrpc": "2.0", "method": "exit", "params": null}
+```
--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,17 @@
-//! A minimal example LSP server that can only respond to the `gotoDefinition` request. To use
-//! this example, execute it and then send an `initialize` request.
-//!
-//! ```no_run
-//! Content-Length: 85
-//!
-//! {"jsonrpc": "2.0", "method": "initialize", "id": 1, "params": {"capabilities": {}}}
-//! ```
-//!
-//! This will respond with a server response. Then send it a `initialized` notification which will
-//! have no response.
-//!
-//! ```no_run
-//! Content-Length: 59
-//!
-//! {"jsonrpc": "2.0", "method": "initialized", "params": {}}
-//! ```
-//!
-//! Once these two are sent, then we enter the main loop of the server. The only request this
-//! example can handle is `gotoDefinition`:
-//!
-//! ```no_run
-//! Content-Length: 159
-//!
-//! {"jsonrpc": "2.0", "method": "textDocument/definition", "id": 2, "params": {"textDocument": {"uri": "file://temp"}, "position": {"line": 1, "character": 1}}}
-//! ```
-//!
-//! To finish up without errors, send a shutdown request:
-//!
-//! ```no_run
-//! Content-Length: 67
-//!
-//! {"jsonrpc": "2.0", "method": "shutdown", "id": 3, "params": null}
-//! ```
-//!
-//! The server will exit the main loop and finally we send a `shutdown` notification to stop
-//! the server.
-//!
-//! ```
-//! Content-Length: 54
-//!
-//! {"jsonrpc": "2.0", "method": "exit", "params": null}
-//! ```
 use std::error::Error;
 
-use lsp_types::OneOf;
 use lsp_types::{
     request::GotoDefinition, GotoDefinitionResponse, InitializeParams, ServerCapabilities,
 };
+use lsp_types::{HoverProviderCapability, Location, OneOf, Position, Range, Url};
 
 use lsp_server::{Connection, ExtractError, Message, Request, RequestId, Response};
 
 fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
-    // Note that  we must have our logging only write out to stderr.
-    eprintln!("starting generic LSP server");
-
-    // Create the transport. Includes the stdio (stdin and stdout) versions but this could
-    // also be implemented to use sockets or HTTP.
     let (connection, io_threads) = Connection::stdio();
-
-    // Run the server and wait for the two threads to end (typically by trigger LSP Exit event).
     let server_capabilities = serde_json::to_value(&ServerCapabilities {
         definition_provider: Some(OneOf::Left(true)),
+        hover_provider: Some(HoverProviderCapability::Simple(true)),
         ..Default::default()
     })
     .unwrap();
@@ -73,11 +24,11 @@ fn main() -> Result<(), Box<dyn Error + Sync + Send>> {
             return Err(e.into());
         }
     };
+
     main_loop(connection, initialization_params)?;
+
     io_threads.join()?;
 
-    // Shut down gracefully.
-    eprintln!("shutting down server");
     Ok(())
 }
 
@@ -85,20 +36,46 @@ fn main_loop(
     connection: Connection,
     params: serde_json::Value,
 ) -> Result<(), Box<dyn Error + Sync + Send>> {
+    let locations: Vec<lsp_types::Location> = vec![
+        Location {
+            uri: Url::parse("file:/home/rhydian/dotfiles/nvim/after/plugin/lspconfig.lua")?,
+            range: Range {
+                start: Position {
+                    line: 1,
+                    character: 1,
+                },
+                end: Position {
+                    line: 1,
+                    character: 1,
+                },
+            },
+        },
+        Location {
+            uri: Url::parse("file:/home/rhydian/dotfiles/nvim/after/plugin/lspconfig.lua")?,
+            range: Range {
+                start: Position {
+                    line: 1,
+                    character: 1,
+                },
+                end: Position {
+                    line: 1,
+                    character: 1,
+                },
+            },
+        },
+    ];
+
     let _params: InitializeParams = serde_json::from_value(params).unwrap();
-    eprintln!("starting example main loop");
     for msg in &connection.receiver {
-        eprintln!("got msg: {msg:?}");
         match msg {
             Message::Request(req) => {
                 if connection.handle_shutdown(&req)? {
                     return Ok(());
                 }
-                eprintln!("got request: {req:?}");
-                match cast::<GotoDefinition>(req) {
+                match cast::<GotoDefinition>(req.clone()) {
                     Ok((id, params)) => {
                         eprintln!("got gotoDefinition request #{id}: {params:?}");
-                        let result = Some(GotoDefinitionResponse::Array(Vec::new()));
+                        let result = Some(GotoDefinitionResponse::Array(locations.clone()));
                         let result = serde_json::to_value(&result).unwrap();
                         let resp = Response {
                             id,
@@ -111,7 +88,22 @@ fn main_loop(
                     Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
                     Err(ExtractError::MethodMismatch(req)) => req,
                 };
-                // ...
+                match cast::<lsp_types::request::HoverRequest>(req.clone()) {
+                    Ok((id, params)) => {
+                        eprintln!("got hover request #{id}: {params:?}");
+                        let result = Some("Hello, world!".to_string());
+                        let result = serde_json::to_value(&result).unwrap();
+                        let resp = Response {
+                            id,
+                            result: Some(result),
+                            error: None,
+                        };
+                        connection.sender.send(Message::Response(resp))?;
+                        continue;
+                    }
+                    Err(err @ ExtractError::JsonError { .. }) => panic!("{err:?}"),
+                    Err(ExtractError::MethodMismatch(req)) => req,
+                };
             }
             Message::Response(resp) => {
                 eprintln!("got response: {resp:?}");
